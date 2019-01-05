febd904edabd9dd3766eacbdb227b5157f06d604
GuoHao <guohaoice@gmail.com>
2019-01-05 22:00:37 +0800

    Add Dubbo context information BLOG (#222)

--- site_config/blog.js
+++ site_config/blog.js
@@ -192,6 +192,13 @@ export default {
         link: '/zh-cn/blog/dubbo-rest.html',
       },
         {
+          title: 'Dubbo 上下文信息',
+          author:'@guohao',
+          dateStr: 'Dec 29th, 2018',
+          desc: '本文介绍了Dubbo框架上下文信息的应用场景和使用方式。',
+          link: '/zh-cn/blog/dubbo-context-information.html',
+        },
+        {
           title: '当Dubbo遇上Arthas：排查问题的实践',
           author:'@hengyunabc',
           dateStr: 'Dec 14th, 2018',
@@ -218,7 +225,7 @@ export default {
             dateStr: 'Sep 25th, 2018',
             desc: 'Dubbo在Service Mesh下的思考和方案',
             link: '/zh-cn/blog/dubbo-mesh-in-thinking.html',
-        },	
+        },
         {
             title: 'Dubbo Mesh ｜ Service Mesh的实践与探索',
             author:'@liyun',
