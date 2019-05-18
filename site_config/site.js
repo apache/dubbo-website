5e66b18ccc7e98e1f42378ec273b03306b8deea5
Huxing Zhang <huxing.zhang@gmail.com>
2019-05-18 09:35:56 +0800

    Remove incubating word

--- site_config/site.js
+++ site_config/site.js
@@ -38,8 +38,8 @@ export default {
       },
     ],
     disclaimer: {
-      title: 'Disclaimer',
-      content: 'Apache Dubbo is an effort undergoing incubation at The Apache Software Foundation (ASF), sponsored by the Incubator. Incubation is required of all newly accepted projects until a further review indicates that the infrastructure, communications, and decision making process have stabilized in a manner consistent with other successful ASF projects. While incubation status is not necessarily a reflection of the completeness or stability of the code, it does indicate that the project has yet to be fully endorsed by the ASF.',
+      title: '',
+      content: '',
     },
     asf: {
       title: 'ASF',
@@ -144,8 +144,8 @@ export default {
       },
     ],
     disclaimer: {
-      title: 'Disclaimer',
-      content: 'Apache Dubbo is an effort undergoing incubation at The Apache Software Foundation (ASF), sponsored by the Incubator. Incubation is required of all newly accepted projects until a further review indicates that the infrastructure, communications, and decision making process have stabilized in a manner consistent with other successful ASF projects. While incubation status is not necessarily a reflection of the completeness or stability of the code, it does indicate that the project has yet to be fully endorsed by the ASF.',
+      title: '',
+      content: '',
     },
     asf: {
       title: 'ASF',
